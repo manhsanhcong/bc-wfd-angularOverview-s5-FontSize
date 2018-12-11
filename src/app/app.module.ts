9c3055ace823aecd9b6c9f4272ba0c977464aec7
manhsanhcong <manhsanhcong.08@gmail.com>
2018-12-11 10:52:28 +0700

    add file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontSizeComponent } from './font-size/font-size.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FontSizeComponent
   ],
   imports: [
     BrowserModule,
